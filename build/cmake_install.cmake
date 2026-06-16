eb8af48c2f112d4e41802a7f3da54c0c06931ec1
Dolle Jan (PEA6-Fe-FI) <oqj1fe@bosch.com>
2026-06-16 12:44:49 +0200

    wtf

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,6 +44,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("C:/Users/OQJ1FE/Desktop/Aufgabe2/build/_deps/glad-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("C:/Users/OQJ1FE/Desktop/Aufgabe2/build/bin/cmake_install.cmake")
 endif()
 
